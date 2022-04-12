db7c2913fdd1045fba109d0ded36912817195a0f
ksamy90 <ksamy90@aol.com>
2022-04-12 10:02:02 +0300

    add get requests for users

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
 const Task = require("./models/task");
+const User = require("./models/user");
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -17,6 +18,45 @@ app.post("/tasks", (req, res) => {
     })
     .catch((error) => {
       res.status(400).send(error);
+    });
+});
+
+app.post("/users", (req, res) => {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.status(200).send(user);
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+});
+
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(500).send();
     });
 });
 
